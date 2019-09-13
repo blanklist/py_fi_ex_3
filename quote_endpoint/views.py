244291bc2fb7706ba45f6a35445a32f644685afc
blanklist <joehineriv@gmail.com>
2019-09-12 19:34:22 -0700

    converted to json then to dict

--- quote_endpoint/views.py
+++ quote_endpoint/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render
 
 from .models import Quote
 
-import urllib.request
+# import urllib.request
+import json
+import requests
 
 def index(request):
-    DJIA_futures = urllib.request.urlopen("https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=YM=F&apikey=K8M3E4D6ZBNJHHHF")
+    API_call_futures = requests.get("https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=YM=F&apikey=K8M3E4D6ZBNJHHHF")
+    API_call_to_json = API_call_futures.json()
+    DJIA_futures = API_call_to_json['Global Quote']
+    
     context = {'DJIA_futures': DJIA_futures}
     return render(request, 'quote_endpoint/index.html', context)
